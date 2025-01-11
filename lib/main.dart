a6d70144bf392ece42a89c500c721554445d6d45
MOMEN56 <5momenalaa5@gmail.com>
2025-01-12 01:22:33 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Flash());
+}
+
+class Flash extends StatelessWidget {
+  const Flash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Expanded(
+          child: Container(
+          color: Color(0xff100B20),
+          ),
+        ),
+      ),
+    );
+  }
+}
